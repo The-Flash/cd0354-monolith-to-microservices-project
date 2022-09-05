301914d4a8459d76bed30adf8282a652bdbb0abc
Augustine Smith <augustine.smith@amalitech.org>
2022-09-05 21:53:04 +0000

    udagram project

--- udagram-api-feed/src/controllers/v0/index.router.ts
+++ udagram-api-feed/src/controllers/v0/index.router.ts
@@ -3,7 +3,7 @@ import {FeedRouter} from './feed/routes/feed.router';
 
 const router: Router = Router();
 
-router.use('/feed', FeedRouter);
+router.use('/', FeedRouter);
 
 router.get('/', async (req: Request, res: Response) => {
   res.send(`V0`);
